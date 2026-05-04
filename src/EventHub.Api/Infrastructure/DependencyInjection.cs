2c5a3b4f8a983fb6e73f788f9f405a2ff9ac5ed2
Dzianis Staliarou <stolyarov_d@sminex.com>
2026-05-04 11:58:47 +0300

    Add in memory event repository.

---
+++ src/EventHub.Api/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using EventHub.Api.Domain.Interfaces;
+using EventHub.Api.Infrastructure.Repositories;
+
+namespace EventHub.Api.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IEventRepository, InMemoryEventRepository>();
+
+        return services;
+    }
+}
