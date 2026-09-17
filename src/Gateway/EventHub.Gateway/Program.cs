f8e7835087dc62fd6cbca60ce21b1f648aa7bd0b
Dzianis Staliarou <stolyarov_d@sminex.com>
2026-09-17 19:17:55 +0300

    Extract gateway service.

---
+++ src/Gateway/EventHub.Gateway/Program.cs
@@ -0,0 +1,19 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+WebApplication app = builder.Build();
+
+app.MapReverseProxy();
+
+app.UseSwaggerUI(options =>
+{
+    options.DocumentTitle = "EventHub API Gateway";
+    options.SwaggerEndpoint("/docs/users/swagger/v1/swagger.json", "Users API v1");
+    options.SwaggerEndpoint("/docs/events/swagger/v1/swagger.json", "Events API v1");
+    options.SwaggerEndpoint("/docs/bookings/swagger/v1/swagger.json", "Bookings API v1");
+});
+
+await app.RunAsync();
